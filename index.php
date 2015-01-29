539fbd3fbda695e8c7dc991ca442bcea9c5f55e3
Shathee <sat.sust@gmail.com>
2015-01-30 02:06:43 +0600

    V1.001

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Ignis Avis Inc.</title>
+
+    <!-- Bootstrap -->
+    <link href="dist/css/bootstrap.css" rel="stylesheet">
+    <link href="dist/css/custom.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="container">
+    	<div class="row">
+			
+		  <div class="col-md-12">
+			<nav class="navbar navbar-inverse">
+			  <div class="container-fluid">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+				  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				  </button>
+				  <a class="navbar-brand" href="#">Home</a>
+				</div>
+
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				  <ul class="nav navbar-nav">
+					<li class=""><a href="#">About <span class="sr-only">(current)</span></a></li>
+					<li><a href="#">Contact</a></li>
+					
+				  </ul>
+				  <form class="navbar-form navbar-right" role="search">
+					<div class="form-group">
+					  <input type="text" class="form-control" placeholder="Search">
+					</div>
+					
+				  </form>
+				  
+				</div><!-- /.navbar-collapse -->
+			  </div><!-- /.container-fluid -->
+			</nav>
+		  </div>
+		  <div class="col-md-12" id="header">
+			
+		  </div>
+	 
+		</div>
+	
+		<div class="row">
+			<div class="col-md-2"></div>
+			
+			  <div class="col-md-8">
+				<center><img src="construction.png" class="img-responsive" alt="Responsive image" /> </center>
+			  </div>
+			<div class="col-md-2"></div>
+		 
+		</div>
+	
+    
+    </div>
+    
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ staff/cv_sat.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Shathee Ruhunnabi.</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+	
+	
+	
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="container">
+    	<div class="row">
+		  
+		  <div class="col-md-12" id="header">
+			
+		  </div>
+		  
+		  
+		  <div class="col-md-8">
+			<h1>
+			Shathee Ruhunnabi
+			<small><h4>PHP Programmer | Procurement Specialist</h4></small>
+			</h1>
+			<p>
+					I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me and you can start adding your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to write more. Tell a story and let your users get to know you.
+				</p>
+		  </div>
+		  <div class="col-md-2">
+			<img src="img/1.png" alt="..." class="img-circle" />
+			
+			
+		  </div>
+		  <div class="col-md-2">
+				<ul class="nav nav-pills nav-stacked">
+				  <li role="presentation" class="text-right"><a href="#">About</a></li>
+				  <li role="presentation" class="text-right"><a href="#">Resume</a></li>
+				  <li role="presentation" class="text-right"><a href="#">Works</a></li>
+				  
+				</ul>
+		  </div>
+		</div>
+	
+		<div class="row">
+			<div class="col-md-8">
+				
+			</div>
+			<div class="col-md-4">
+			
+			</div>
+		</div>
+		
+		<div class="row" id="footer">
+			<div class="col-md-3">
+				<h2><i class="glyphicon glyphicon-earphone" ></i> Call</h2>
+				<h4>01911130473</h4>
+				<h4>01680846364</h4>
+				<address>
+				  <h4><a href="mailto:#">first.last@example.com</a></h4>
+				</address>
+				
+				
+			</div>
+			<div class="col-md-3">
+				<h1 class="glyphicon glyphicon-map-marker" ></h1>
+				<address>
+				  <strong>53, Tejkunipara (3rd Floor).</strong><br>
+				  Hondar Goli<br>
+				  Tejgaon, dhaka-1215<br>
+				  
+				</address>
+	
+				
+			</div>
+			<div class="col-md-3">
+				<h1 class="glyphicon glyphicon-copyright-mark" ></h1>
+				<h4>© 2015 by Shathee Ruhunnabi</h4>
+				
+				
+			</div>
+			<div class="col-md-3">
+				<h1 class="" >&nbsp;</h1>
+				<a href="https://www.facebook.com/Ruhunnabi" class="img-thumbnail">
+					<i class="fa fa-facebook fa-2x"> </i>
+				</a>
+				<a href=""  class="img-thumbnail"><i class="fa fa-twitter fa-2x"> </i></a>
+				<a href=""  class="img-thumbnail"><i class="fa fa-google-plus fa-2x"> </i></a>
+				
+				
+			</div>
+		</div>
+    
+    </div>
+    
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
